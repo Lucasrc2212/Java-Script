21b386e8bfc20aaa25c35debaa3dbfb8f6666d14
Lucas Rottini Cruz <162641853+Lucasrc2212@users.noreply.github.com>
2025-02-27 19:52:36 -0300

    Add files via upload

---
+++ Lucas Cruz 22/aula2.js
@@ -0,0 +1,11 @@
+
+function teste(){
+    nome = document.getElementById('nome').value
+    alert('Olá '+nome)
+}
+
+function soma(){
+    n1 = parseInt(document.getElementById('n1').value)
+    n2 = parseInt(document.getElementById('n2').value)
+    alert('A some deu '+(n1+n2))
+}

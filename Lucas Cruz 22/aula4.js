3e85f159e5020261504550d3190b283be26818db
Lucas Rottini Cruz <162641853+Lucasrc2212@users.noreply.github.com>
2025-04-03 17:49:26 -0300

    Update aula4.js

--- Lucas Cruz 22/aula4.js
+++ Lucas Cruz 22/aula4.js
@@ -5,5 +5,7 @@ function clicar(){
     nomes = document.getElementById('texto').value 
     frutas.push(nomes)
     document.getElementById('lista').innerHTML += '<li>'+nomes+'</li>'
+    quantidade = frutas.length().value
+    document.getElementById('contador').innerHTML = `A lista tem ${quantidade} frutas`
 
 }
